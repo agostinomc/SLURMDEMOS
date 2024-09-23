acbba334d63316db7deb65faeed8a4d8a13d961d
agostinomc <169169524+agostinomc@users.noreply.github.com>
2024-09-23 15:13:34 +0200

    Update submit_train.sh

--- code/submit_train.sh
+++ code/submit_train.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=train-nn-gpu
-#SBATCH -t 00:20:00                  # estimated time # TODO: adapt to your needs/ the full training run will take approx. 5 h on an A100
-#SBATCH -p kisski              # the partition you are training on (i.e., which nodes), for nodes see sinfo -o "%25N  %5c  %10m  %32f  %10G %18P " | grep gpu
-#SBATCH -G A100:1                   # requesting GPU slices, see https://docs.hpc.gwdg.de/usage_guide/slurm/gpu_usage/index.html for more options
+#SBATCH -t 00:20:00                  # estimated time # TODO: adapt to your needs
 #SBATCH --nodes=1                    # total number of nodes
 #SBATCH --ntasks=1                   # total number of tasks
 #SBATCH --cpus-per-task 4            # number of CPU cores per task
-#SBATCH --mail-type=all              # send mail when job begins and ends
 #SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
 #SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
-# load new software stack
-# see https://docs.hpc.gwdg.de/software_stacks/index.html
+
 export PREFERRED_SOFTWARE_STACK=nhr-lmod
 source /sw/etc/profile/profile.sh
 
