79a679c2f09ea4cb6d7da154903589d7357d9285
agostinomc <169169524+agostinomc@users.noreply.github.com>
2024-09-23 15:14:27 +0200

    Update submit_test.sh

--- code/submit_test.sh
+++ code/submit_test.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=test-nn-gpu
 #SBATCH -t 00:10:00                  # estimated time # TODO: adapt to your needs
-#SBATCH -p kisski                    # the partition you are training on (i.e., which nodes), for nodes see sinfo -o "%25N  %5c  %10m  %32f  %10G %18P " | grep gpu
-#SBATCH -G A100:1          # requesting GPU slices, see https://docs.hpc.gwdg.de/usage_guide/slurm/gpu_usage/index.html for more options
 #SBATCH --nodes=1                    # total number of nodes
 #SBATCH --ntasks=1                   # total number of tasks
-#SBATCH --cpus-per-task 4            # number of CPU cores per task
-#SBATCH --mail-type=all              # send mail when job begins and ends
 #SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
 #SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
-# load new software stack
-# see https://docs.hpc.gwdg.de/software/software_stacks/index.html
-export PREFERRED_SOFTWARE_STACK=nhr-lmod
-source /sw/etc/profile/profile.sh
 
 module load miniconda3
 module load cuda
