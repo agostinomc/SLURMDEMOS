db9dc4410c8153281c29850ec2ab2602f43b6778
agostinomc <169169524+agostinomc@users.noreply.github.com>
2024-09-23 10:08:05 +0200

    Update build_dlc-dlwgpu.sh

--- code/container/build_dlc-dlwgpu.sh
+++ code/container/build_dlc-dlwgpu.sh
@@ -2,8 +2,7 @@
 module load apptainer
 #export APPTAINER_CACHEDIR=$WORK/.apptainer/cache
 
-# define software stack that will become default soon (September 2024)
-# see https://docs.hpc.gwdg.de/software/nhr_lmod/index.html
+
 export PREFERRED_SOFTWARE_STACK=nhr-lmod
 source /sw/etc/profile/profile.sh
 
